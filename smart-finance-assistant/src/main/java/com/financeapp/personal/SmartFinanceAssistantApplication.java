84471c7a0158493305b762f484bd687a0c735511
genecheney <genecheney@hotmail.com>
2025-07-03 13:37:18 -0500

    init commit

---
+++ smart-finance-assistant/src/main/java/com/financeapp/personal/SmartFinanceAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.financeapp.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinanceAssistantApplication.class, args);
+	}
+
+}
---
+++ smart-finance-assistant/src/test/java/com/financeapp/personal/SmartFinanceAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeapp.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
